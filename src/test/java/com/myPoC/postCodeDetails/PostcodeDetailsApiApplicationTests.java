ad89511846481d6a26818358bb4926470a3223a4
NeethuTom <37003548+NeethuTom@users.noreply.github.com>
2021-02-28 21:04:41 +1100

    Create PostcodeDetailsApiApplicationTests.java

---
+++ src/test/java/com/myPoC/postCodeDetails/PostcodeDetailsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myPoC.postCodeDetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostcodeDetailsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
