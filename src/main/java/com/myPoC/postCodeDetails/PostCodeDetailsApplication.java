b65a79a408ea8dc50187744f72ff0fc63f05553f
NeethuTom <37003548+NeethuTom@users.noreply.github.com>
2021-02-28 21:02:37 +1100

    Add files via upload

---
+++ src/main/java/com/myPoC/postCodeDetails/PostCodeDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.myPoC.postCodeDetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostCodeDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostCodeDetailsApplication.class, args);
+	}
+
+}
